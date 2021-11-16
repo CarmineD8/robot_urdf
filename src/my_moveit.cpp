64975059a7ea3590d62dcd04c4f57347de2e6453
root <carmine.recchiuto@dibris.unige.it>
2021-11-16 12:45:54 +0000

    code updated

--- src/my_moveit.cpp
+++ src/my_moveit.cpp
@@ -1,66 +1,153 @@
 #include <ros/ros.h>
-//MOVE IT
+
+// MoveIt
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "robot_model_and_robot_state_tutorial");
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  
+  // We start by instantiating a
+  // `RobotModelLoader`_
+  // object, which will look up
+  // the robot description on the ROS parameter server and construct a
+  // :moveit_core:`RobotModel` for us to use.
+  //
+  // .. _RobotModelLoader:
+  //     http://docs.ros.org/noetic/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
+  
+  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+  const moveit::core::RobotModelPtr& kinematic_model = robot_model_loader.getModel();
+  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
 
-ros::init(argc, argv, "custom_planning");
-ros::NodeHandle nh;
-ros::AsyncSpinner spinner(1);
-spinner.start();
-sleep(2.0);
+  // Using the :moveit_core:`RobotModel`, we can construct a
+  // :moveit_core:`RobotState` that maintains the configuration
+  // of the robot. We will set all joints in the state to their
+  // default values. We can then get a
+  // :moveit_core:`JointModelGroup`, which represents the robot
+  // model for a particular group, e.g. the "panda_arm" of the Panda
+  // robot.
+  
+  moveit::core::RobotStatePtr kinematic_state(new moveit::core::RobotState(kinematic_model));
+  kinematic_state->setToDefaultValues();
+  const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("arm");
+  moveit::planning_interface::MoveGroupInterface group("arm");
+  const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
 
-moveit::planning_interface::MoveGroupInterface group("arm");
-group.setEndEffectorLink("arm_link_04");
-group.setPoseReferenceFrame("base_link");
-group.setPlannerId("RRTstar");
-group.setNumPlanningAttempts(10);
-group.setPlanningTime(10.0);
-group.allowReplanning(true);
-group.setGoalJointTolerance(0.0001);
-group.setGoalPositionTolerance(0.0001);
-group.setGoalOrientationTolerance(0.001);
-int choice;
+  
+  //kinematic_state->setToRandomPositions(joint_model_group);
+  //const Eigen::Isometry3d& pose1 = kinematic_state->getGlobalLinkTransform("arm_link_04");
+  geometry_msgs::Pose pose1;
+	
+  pose1.orientation.w = 0.70;
+  pose1.orientation.x = -0.00;
+  pose1.orientation.y = 0.00;
+  pose1.orientation.z = -0.71;
+  pose1.position.x =  0.50;
+  pose1.position.y =  0.00;
+  pose1.position.z =  1.15;
+  
+  
+  // Inverse Kinematics
+  // ^^^^^^^^^^^^^^^^^^
+  // We can now solve inverse kinematics (IK) for the Panda robot.
+  // To solve IK, we will need the following:
+  //
+  //  * The desired pose of the end-effector 
+  //  * The timeout: 0.1 s
+  
+  group.setStartStateToCurrentState();
+  group.setApproximateJointValueTarget(pose1, "arm_link_04");
+  std::vector<double> joint_values;
+  double timeout = 0.1;
+  bool found_ik = kinematic_state->setFromIK(joint_model_group, pose1, timeout);
 
-while (ros::ok()){
+  // Now, we can print out the IK solution (if found):
+  if (found_ik)
+  {
+    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+    for (std::size_t i = 0; i < joint_names.size(); ++i)
+    {
+      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+    }
+  }
+  else
+  {
+    ROS_INFO("Did not find IK solution");
+  }
+  
+  
+  group.setJointValueTarget(joint_values);
+  group.setStartStateToCurrentState();
+  group.setGoalOrientationTolerance(0.01);
+  group.setGoalPositionTolerance(0.01);
 
-std::cout << "Press 1 to reach the zero position, press 2 to reach a different position: ";
-std::cin >> choice;
-
-if (choice == 1){
-
+  // Plan and execute
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+  group.plan(my_plan); 
+  group.execute(my_plan);
+  
+  std::cout << "Position 1 -> IK + setJointValue" << std::endl;
+  sleep(5.0);
+  
+  // Move to default positions
+  
 	group.setNamedTarget("zero");
 	group.move();  
-	sleep(5.0);
-}
+	
+   std::cout << "Position 2 -> Zero" << std::endl;
+   sleep(5.0);
+	
+	group.setNamedTarget("pose1");
+	group.move();   
+	
+   std::cout << "Position 3 -> Pose1" << std::endl;
+   sleep(5.0);
+	
+  pose1.orientation.w = 0.00;
+  pose1.orientation.x = -0.00;
+  pose1.orientation.y = 0.00;
+  pose1.orientation.z = 1.00;
+  pose1.position.x =  0.50;
+  pose1.position.y =  0.50;
+  pose1.position.z =  1.00;
+  
+  group.setStartStateToCurrentState();
+  group.setApproximateJointValueTarget(pose1, "arm_link_04");
+  found_ik = kinematic_state->setFromIK(joint_model_group, pose1, timeout);
 
-else if (choice == 2){
+  if (found_ik)
+  {
+    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+    for (std::size_t i = 0; i < joint_names.size(); ++i)
+    {
+      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+    }
+  }
+  else
+  {
+    ROS_INFO("Did not find IK solution");
+  }
+  
+  
+  group.setJointValueTarget(joint_values);
+  group.setStartStateToCurrentState();
+  group.setGoalOrientationTolerance(0.01);
+  group.setGoalPositionTolerance(0.01);
 
-	geometry_msgs::Pose target_pose1;
-	//This should be a valid pose 
-	target_pose1.orientation.w = 1.0;
-	target_pose1.orientation.x = 0.0;
-	target_pose1.orientation.y = 0.0;
-	target_pose1.orientation.z = 0.0;
-	target_pose1.position.x =  0.1;
-	target_pose1.position.y =  0.0;
-	target_pose1.position.z =  0.2;
+  // Plan and execute
+  group.plan(my_plan); 
+  group.execute(my_plan);
+  
+  std::cout << "Position 4 -> IK + setJointValue" << std::endl;
+  sleep(5.0);
+  
 
-
-	group.setStartStateToCurrentState();
-	group.setApproximateJointValueTarget(target_pose1, "arm_link_04");
-
-	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-	group.plan(my_plan);
-	group.execute(my_plan);
-
-	sleep(5.0);
-}
-}
-
-ros::shutdown();
-
-return 0;
+  ros::shutdown();
+  return 0;
 }
